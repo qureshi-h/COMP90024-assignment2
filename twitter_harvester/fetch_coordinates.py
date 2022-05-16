ac4096a0b5c9620040b71af97a3eb778b7466e71
Hamza <hamza.ahmedqureshi@hotmail.com>
2022-05-16 12:18:06 +1000

    added region in harvester

---
+++ twitter_harvester/fetch_coordinates.py
@@ -0,0 +1,23 @@
+import geopandas as gpd
+from shapely.geometry import Point
+
+
+def get_region(coordinates, option="lga"):
+    coordinates = Point(coordinates)
+
+    shape_file = "../res/SA3_2021_AUST_GDA2020.shp"
+    if option == "lga":
+        shape_file = '../res/vic_lga.shp'
+
+    gdf = gpd.read_file(shape_file)
+    gdf = gdf.to_crs(epsg=4326)
+    for row in gdf.iterrows():
+        if row[1]["geometry"] is not None and row[1]["geometry"].contains(coordinates):
+            if option == "lga":
+                return row[1]["LGA_NAME"]
+            return row[1]["SA3_NAME21"]
+
+    return False
+
+
+# print(get_region((35, -144)))
--- twitter_harvester/twitter_streaming.py
+++ twitter_harvester/twitter_streaming.py
@@ -1,22 +1,25 @@
+from ast import keyword
+from calendar import c
 import os
 import time
 from dotenv import load_dotenv
+from shapely.geometry import Polygon, Point
 
 import couchdb
 import requests
 
-# import fetch_coordinates
+import fetch_coordinates
 import stem_keywords
-
-KEYWORDS = stem_keywords.get_keywords()
 
 load_dotenv()
 
 
 def main():
+
+    keywords, bounding_box = setup()
+
     headers = create_headers(os.getenv('BEARER_TOKEN'))
-    keyword = combine_keywords() + " lang:en"
-    print(len(keyword))
+
     max_results = 100
     limit = 1000
     next_token = None
@@ -25,14 +28,16 @@ def main():
     password = os.getenv("COUCHDB_PASSWORD")
     server = couchdb.Server("http://%s:%s@172.26.133.72:5984/" % (username, password))
 
-    url = create_url(keyword, max_results)
+    url = create_url(keywords, max_results)
 
     response = connect_to_endpoint(url[0], headers, url[1], next_token)
-    counter = process_tweet(response, server)
+    counter = process_tweet(response, server, bounding_box)
     refresh_url = response['search_metadata']["refresh_url"]
+    
     while counter < limit:
+        print(counter, flush=True)
         response = request_url(refresh_url, headers)
-        counter += process_tweet(response, server)
+        counter += process_tweet(response, server, bounding_box)
 
         refresh_url = response['search_metadata']["refresh_url"]
         time.sleep(10)
@@ -40,25 +45,57 @@ def main():
     print("\n", counter, "tweets found!")
 
 
-def process_tweet(response, server):
+
+def setup():
+
+    default_box = "143.7832,-38.5375,146.1406,-37.0837"
+
+    if os.getenv("KEYWORDS") != "None":
+        if not len(os.getenv("KEYWORDS")):
+            keywords = ""
+        else:
+            keywords = "(" + " OR ".join(os.getenv("KEYWORDS").split(" ")) + ")"
+    else:
+        keywords = combine_keywords()
+
+    try:
+        bounding_coordinates = os.getenv("BOUNDING_BOX")      
+        points = list(map(float, bounding_coordinates.split(",")))
+        bounding_box = Polygon(((points[2], points[1]), (points[0], points[1]),
+         (points[2], points[3]), (points[0], points[3])))
+    except:
+        points = list(map(float, default_box.split(",")))
+        bounding_box = Polygon(((points[2], points[1]), (points[0], points[1]),
+         (points[2], points[3]), (points[0], points[3])))
+
+    print("Keywords:", keywords, flush=True)
+    print("Bounding Box:", bounding_box, flush=True)
+
+    return keywords, bounding_box
+    
+def process_tweet(response, server, bounding_box):
     for tweet in response["statuses"]:
         if tweet["coordinates"]:
+            print(tweet["coordinates"], flush=True)
+            if not Point(list(map(float, tweet["coordinates"]["coordinates"]))).within(bounding_box):
+                continue
+            print("Found!", tweet["text"], flush=True)
             categories = categorize(tweet["text"])
-            # region = fetch_coordinates.get_region(tweet["doc"]["coordinates"]["coordinates"])
-            region = "Melbourne"
+            region = fetch_coordinates.get_region(tweet["doc"]["coordinates"]["coordinates"])
             if not categories or not region:
                 return
             for category in categories:
                 server["tweets"]({"region": region, "type": category[0], "subtype": category[1],
                                   "tweet": tweet["text"], "coordinates": tweet["coordinates"]["coordinates"]})
                 print({"region": region, "type": category[0], "subtype": category[1],
-                       "tweet": tweet["text"], "coordinates": tweet["coordinates"]["coordinates"]})
+                       "tweet": tweet["text"], "coordinates": tweet["coordinates"]["coordinates"]}, flush=True)
     return len(response["statuses"])
 
 
 def categorize(tweet):
     categories = []
-    for topic, subtopics in KEYWORDS.items():
+    keywords = stem_keywords.get_keywords()
+    for topic, subtopics in keywords.items():
         for subtopic, words in subtopics.items():
             for word in words:
                 if word in tweet:
@@ -77,7 +114,7 @@ def create_url(keyword, max_results=10):
     search_url = "https://api.twitter.com/1.1/search/tweets.json"
 
     query_params = {'q': keyword,
-                    'geocode': '-37.840935,144.946457,5000km',
+                    'geocode': '-37.840935,144.946457,500km',
                     "count": 100}
 
     return search_url, query_params
@@ -109,7 +146,8 @@ def get_coordinates(place_id, headers):
 
 def combine_keywords():
 
-    return "(" + " OR ".join([topic for key in KEYWORDS for topic in KEYWORDS[key]]) + ")"
+    words = stem_keywords.get_keywords()
+    return "(" + " OR ".join([topic for key in words for topic in words[key]]) + ")" + " lang:en"
 
 
 main()
